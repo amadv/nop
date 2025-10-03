ac6dea83638e96474d7342cbad35bc28232c1d3e
amadv <aaronmadved@gmail.com>
2025-10-03 11:29:56 -0400

    fix from fresh install

--- setup_nop.pl
+++ setup_nop.pl
@@ -3,9 +3,9 @@ use strict;
 use warnings;
 use File::Path qw(make_path);
 use File::Spec;
-use File::HomeDir;
+use Cwd 'abs_path';
 
-my $home    = File::HomeDir->my_home;
+my $home    = $ENV{HOME};
 my $notes   = File::Spec->catdir($home, "Notes");
 my $localbin = File::Spec->catdir($home, ".local", "bin");
 my $script   = File::Spec->catfile($notes, "nop.pl");
@@ -24,16 +24,25 @@ unless (-d $localbin) {
 }
 
 # 3. If nop.pl doesn’t exist, create a stub script
-unless (-f $script) {
-    print "Creating sample $script...\n";
-    open my $fh, ">", $script or die "Cannot create $script: $!";
-    print $fh <<"EOF";
+my $cwd_script = File::Spec->catfile('.', 'nop.pl');
+if (-f $cwd_script) {
+    # Use the one in the current directory
+    $script = abs_path($cwd_script);
+    print "Using existing $script from current directory\n";
+} else {
+    # Fall back to ~/Notes/nop.pl
+    $script = File::Spec->catfile($notes, "nop.pl");
+    unless (-f $script) {
+        print "Creating sample $script...\n";
+        open my $fh, ">", $script or die "Cannot create $script: $!";
+        print $fh <<"EOF";
 #!/usr/bin/env perl
 use strict;
 use warnings;
 print "Hello from nop!\\n";
 EOF
-    close $fh;
+        close $fh;
+    }
 }
 
 # 4. Make nop.pl executable
